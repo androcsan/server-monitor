f44c2117c6cda7db14dd2eac74355d625b5b18bb
Andrei O <andr.ocsan@gmail.com>
2026-03-16 18:55:45 +0800

    CPU, RAM, and Disk Space Added.

---
+++ monitor.py
@@ -0,0 +1,25 @@
+import psutil
+import os
+
+
+def get_system_usage():
+    print("--- Server Resource Monitor")
+
+    cpu_usage = psutil.cpu_percent(interval=1)
+    print(f"* CPU Usage: {cpu_usage}%")
+
+    mem = psutil.virtual_memory()
+    print(f"* Memory Usage: {mem.percent}%")
+    print(f"  - Total: {round(mem.total / (1024**3), 2)} GB")
+    print(f"  - Used: {round(mem.used / (1024**3), 2)} GB")
+    print(f"  - Available: {round(mem.available / (1024**3), 2)} GB")
+
+    disk = psutil.disk_usage(os.sep)
+    print(f"* Disk Usage: {disk.percent}%")
+    print(f"  - Total: {round(disk.total / (1024**3), 2)} GB")
+    print(f"  - Used: {round(disk.used / (1024**3), 2)} GB")
+    print(f"  - Free: {round(disk.free / (1024**3), 2)} GB")
+
+
+if __name__ == "__main__":
+    get_system_usage()
